1714ce4fc3fcb9972f23b1b50cfe943d9e8a2da7
tyrotoxism <jomomojo55@gmail.com>
2024-02-04 19:32:31 -0800

    Adding instruction memory as MIPS_on_Chip_memory

--- src/program_counter.v
+++ src/program_counter.v
@@ -1,12 +1,14 @@
-module program_counter(
+module program_counter
+	#( parameter WIDTH=8)
+	(
 	input wire clk, 
 	input wire reset,
-	input wire [31:0] next_address,
-	output reg [31:0] current_address 
+	input wire [WIDTH-1:0] next_address,
+	output reg [WIDTH-1:0] current_address 
 );
 always @(posedge clk or posedge reset)begin
 	if(reset)
-		current_address <=32'h0;
+		current_address <= 8'h0;	//Change bit width to match WIDTH parameter
 	else
 		current_address <= next_address;
 end
